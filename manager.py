d885ddf2dc8d86fccc73b0f053b7afb4ceb1233e
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-02-10 13:29:12 +0100

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,1 +1,39 @@
-import hashlib,sqlite3 def h(p):return hashlib.sha256(p.encode()).hexdigest() def c(u,p): conn=sqlite3.connect("password_manager.db");c=conn.cursor() c.execute("""CREATE TABLE IF NOT EXISTS accounts (username text PRIMARY KEY,password text)""") c.execute("""INSERT OR IGNORE INTO accounts (username, password) VALUES (?,?)""",(u,h(p))) conn.commit();conn.close();print("Account created!") def l(u,p): conn=sqlite3.connect("password_manager.db");c=conn.cursor() c.execute("""SELECT password FROM accounts WHERE username=?""",(u,)) s=c.fetchone();conn.close();return s and s[0]==h(p) def main(): c("johndoe","password123") r=l("johndoe","password123");print("Login Successful!"if r else"Login Failed.") r=l("johndoe","incorrectpassword");print("Login Successful!"if r else"Login Failed.") if __name__ == "__main__": main()
+import hashlib,os,getpass,sqlite3
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+def password_manager():
+    os.makedirs("accounts",exist_ok=True)
+    os.chdir("accounts")
+    conn=sqlite3.connect("passwords.db")
+    c=conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS passwords (username text,password text)")
+    while True:
+        print("1.Create account\n2.Login\n3.Quit")
+        choice=input("Enter your choice: ")
+        if choice=="1":
+            print("Create new account")
+            username=input("Username: ")
+            password=getpass.getpass("Password: ")
+            hashed_password=hash_password(password)
+            c.execute("INSERT INTO passwords VALUES (?,?)",(username,hashed_password))
+            conn.commit()
+            print("Account created!")
+        elif choice=="2":
+            print("Login")
+            username=input("Username: ")
+            password=getpass.getpass("Password: ")
+            c.execute("SELECT password FROM passwords WHERE username=?",(username,))
+            result=c.fetchone()
+            if result:
+                if hash_password(password)==result[0]:
+                    print("Login successful!")
+                else:
+                    print("Incorrect password")
+            else:
+                print("Account does not exist")
+        elif choice=="3":break
+        else:
+            print("Invalid choice")
+    conn.close()
+if __name__=="__main__":
+    password_manager()
