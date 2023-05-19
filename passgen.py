2d09d95342bb1b1fb8e7e1f17441b4f98f90e08d
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-05-19 13:18:10 +0200

    Update passgen.py
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

--- passgen.py
+++ passgen.py
@@ -1,12 +1,12 @@
 import random,string
-allowed_chars=string.ascii_letters+string.digits+"!$&*+,-/:;=?@^_~"
-num_passwords=int(input("How many passwords do you need? "))
+ac=string.ascii_letters+string.digits+string.punctuation
+np2=int(input("How many passwords do you need? "))
 while True:
-    password_length=int(input("How long should each password be (between 8 and 128 characters)? "))
-    if 8<=password_length<=128:
+    pl=int(input("How long should each password be (between 8 and 128 characters)? "))
+    if 8<=pl<=128:
         break
-    print("Invalid password length, Please enter a value between 8 and 128.")
-passwords=set(''.join(random.choice(allowed_chars)for _ in range(password_length))for _ in range(num_passwords))
-print("\n".join(passwords))
+    print("Invalid password length. Please enter a value between 8 and 128.")
+p2=set(''.join(random.choice(ac)for _ in range(pl))for _ in range(np2))
+print("\n".join(p2))
 with open("passwords.txt","w")as f:
-    f.write("\n".join(passwords))
+    f.write("\n".join(p2))
